04de8360c934c14e371de84300b138472c4898a1
imxaander <xander22.ison@gmail.com>
2022-10-17 19:44:12 +0800

    Added Few Changes
    Added SideBar
     - SideBar Functionality
     -

--- scripts/script.js
+++ scripts/script.js
@@ -19,10 +19,24 @@ function colOrex(){
 function openNavBar(){
     document.getElementById("dimPane").style.display = "block"
 }
+
 function openDim(){
     document.getElementById("dimPane").style.display = "block"
 }
 
 function closeDim(){
     document.getElementById("dimPane").style.display = "none"
+    closeSideBar()
+}
+
+function openSideBar(){
+    document.getElementById("sideBar").style.width = "70%"
+    document.getElementById("sideBar").style.display = "block"
+    openDim()
+}
+
+function closeSideBar(){
+    document.getElementById("sideBar").style.width = "0"
+    document.getElementById("sideBar").style.display = "none"
+    closeDim()
 }
